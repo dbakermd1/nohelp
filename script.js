9cb1277ec7f4edb1423d42b28cbfdaf5cf25bf78
dbakermd1 <dbakermd1@gmail.com>
2017-04-16 17:03:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function countNoOfWords(){
+
+ document.myform.noofwords.value = document.myform.inpString.value.split(' ').length;
+
+}
+function countNoOfSentences(){
+
+  document.myform.noofsentences.value = document.myform.inpString.value.split(' ').length;
+
+}
